4476260591370355a2863f42c81247e989326928
Ewan Chou <coocood@gmail.com>
2015-07-12 19:49:32 +0800

    further fix err check and add test case.

--- server/util_err.go
+++ server/util_err.go
@@ -5,16 +5,12 @@ type causer interface {
 }
 
 func ErrorEqual(err1, err2 error) bool {
-	if e, ok := err1.(causer); ok {
+	if e, ok := err1.(causer); ok && e.Cause() != nil {
 		err1 = e.Cause()
 	}
-	if e, ok := err2.(causer); ok {
+	if e, ok := err2.(causer); ok && e.Cause() != nil {
 		err2 = e.Cause()
 	}
-	if err1 == err2 {
-		return true
-	}
-
 	if err1 == nil || err2 == nil {
 		return err1 == err2
 	}
